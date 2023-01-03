566fd486511813fdc7f556f7b1b35abdf0370de8
Hyun <itsmine096@gmail.com>
2023-01-03 22:38:37 +0900

    🎉 프로젝트 생성

---
+++ src/main/java/com/hyun/TestCodePrac/TestCodePracApplication.java
@@ -0,0 +1,13 @@
+package com.hyun.TestCodePrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCodePracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCodePracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyun/TestCodePrac/TestCodePracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyun.TestCodePrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCodePracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
