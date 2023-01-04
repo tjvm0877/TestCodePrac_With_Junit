a8b90f7285df74d1ba135ccb292b47678036355f
Hyun <itsmine096@gmail.com>
2023-01-04 14:07:10 +0900

    ✅ 책 조회(단일, 목록) 조회 테스트 추가

--- src/test/java/com/hyun/TestCodePrac/repository/BookRepositoryTest.java
+++ src/test/java/com/hyun/TestCodePrac/repository/BookRepositoryTest.java
@@ -2,6 +2,10 @@ package com.hyun.TestCodePrac.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,10 +18,27 @@ class BookRepositoryTest {
 	@Autowired
 	private BookRepository bookRepository;
 
+	@BeforeEach
+	public void dataSet() {
+		String title = "Junit5";
+		String author = "hyun";
+		Book book = Book.builder()
+			.title(title)
+			.author(author)
+			.build();
+		bookRepository.save(book);
+		System.out.println("==================================" + book.getId());
+		List<Book> books = bookRepository.findAll();
+		for (Book onebook : books) {
+			System.out.println(onebook.toString());
+		}
+	}
+
 	// 1. 책 등록하기
 	@Test
+	@DisplayName("책 등록")
 	public void 책_등록하기() {
-		/* given - 데이터 실행 */
+		/* given - 데이터 준비 */
 		String title = "Junit5";
 		String author = "hyun";
 		Book book = Book.builder()
@@ -32,10 +53,40 @@ class BookRepositoryTest {
 		assertEquals(title, bookPS.getTitle());
 		assertEquals(author, bookPS.getAuthor());
 	}
+
 	// 2. 책 목록보기
+	@Test
+	@DisplayName("책 목록조회")
+	public void 책_목록조회() {
+
+		/* given - 데이터 준비 */
+		String title = "Junit5";
+		String author = "hyun";
+
+		/* when - 테스트 실행 */
+		List<Book> booksPS = bookRepository.findAll();
+
+		/* then - 검증 */
+		assertEquals(title, booksPS.get(0).getTitle());
+		assertEquals(author, booksPS.get(0).getAuthor());
+	}
 
 	// 3. 책 한건보기
+	@Test
+	@DisplayName("책 한 건 조회")
+	public void 책_한건보기() {
 
+		/* given - 데이터 준비 */
+		String title = "Junit5";
+		String author = "hyun";
+
+		/* when - 테스트 실행 */
+		Book bookPS = bookRepository.findById(1L).get();
+
+		/* then - 검증 */
+		assertEquals(title, bookPS.getTitle());
+		assertEquals(author, bookPS.getAuthor());
+	}
 	// 4. 책 수정
 
 	// 5. 책 삭제
