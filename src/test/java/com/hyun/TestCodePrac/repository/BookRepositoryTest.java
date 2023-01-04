8d8386c7694fb0174d3847ecd9d6663964581e11
Hyun <itsmine096@gmail.com>
2023-01-04 12:33:36 +0900

    ✅ 책 등록 테스트 코드 추가

--- src/test/java/com/hyun/TestCodePrac/repository/BookRepositoryTest.java
+++ src/test/java/com/hyun/TestCodePrac/repository/BookRepositoryTest.java
@@ -1,8 +1,12 @@
 package com.hyun.TestCodePrac.repository;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.hyun.TestCodePrac.Entity.Book;
 
 @DataJpaTest // DB에 관련된 컴포넌트만 메모리에 로딩
 class BookRepositoryTest {
@@ -13,7 +17,20 @@ class BookRepositoryTest {
 	// 1. 책 등록하기
 	@Test
 	public void 책_등록하기() {
-		System.out.println("책 등록 실행");
+		/* given - 데이터 실행 */
+		String title = "Junit5";
+		String author = "hyun";
+		Book book = Book.builder()
+			.title(title)
+			.author(author)
+			.build();
+
+		/* when - 테스트 실행 */
+		Book bookPS = bookRepository.save(book);
+
+		/* then - 검증 */
+		assertEquals(title, bookPS.getTitle());
+		assertEquals(author, bookPS.getAuthor());
 	}
 	// 2. 책 목록보기
 
