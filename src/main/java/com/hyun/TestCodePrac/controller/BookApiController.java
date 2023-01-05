30a267c630e6d8e40c867c2273b24ca7aa19d108
Hyun <itsmine096@gmail.com>
2023-01-05 15:47:51 +0900

    ✨ 책 수정 기능 controller 추가

--- src/main/java/com/hyun/TestCodePrac/controller/BookApiController.java
+++ src/main/java/com/hyun/TestCodePrac/controller/BookApiController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -80,4 +81,14 @@ public class BookApiController {
 	}
 
 	// 5. 책 수정
+	@PutMapping("/book/{id}")
+	public ResponseEntity<?> updateBook(@PathVariable Long id, @Valid @RequestBody BookSaveReqDto dto) {
+		BookRespDto book = bookService.updateBook(id, dto);
+		CMResponseDto<?> response = CMResponseDto.builder()
+			.result("success")
+			.msg("책 정보 수정 성공")
+			.data(book)
+			.build();
+		return new ResponseEntity<>(response, HttpStatus.CREATED);
+	}
 }
